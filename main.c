c8d954afce639f4ea039d02e82a593e02da1e089
rackhamn <carl.m.blumenthal@gmail.com>
2023-03-15 05:05:50 +0100

    Added SDL window and runtime loop

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// #include <GL/gl.h>
+// #include <GL/glew.h>
 #include <SDL2/SDL.h>
 
 #include "time.c"
@@ -14,6 +16,9 @@ int main(int argc, char ** argv) {
     SDL_Event sdl_event;
     SDL_version sdl_ver_compiled, sdl_ver_linked;
 
+    SDL_Window * window;
+	// SDL_GLContext context;
+
     start = get_time_us();
 
     printf("hello world!\n");
@@ -26,10 +31,31 @@ int main(int argc, char ** argv) {
 
     printf("* compiled against SDL version %u.%u.%u\n", 
             sdl_ver_compiled.major, sdl_ver_compiled.minor, sdl_ver_compiled.patch);
+
     printf("* linked against SDL version %u.%u.%u\n", 
             sdl_ver_linked.major, sdl_ver_linked.minor, sdl_ver_linked.patch);
 
-    sleep_us(1000);
+    printf("create SDL window\n");
+    unsigned int window_flags = SDL_WINDOW_RESIZABLE;
+    window = SDL_CreateWindow("title", 
+                            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                            640, 480, window_flags); 
+
+    while(!quit) {
+        while(SDL_PollEvent(&sdl_event)) {
+		    switch(sdl_event.type) {
+			    case SDL_QUIT:
+				    printf("cmd: sdl_window_quit\n");
+				    quit = 1;
+			    break;
+		    }
+	    }
+
+        sleep_us(1000);
+    }
+
+    printf("Destroy SDL windows\n");
+    SDL_DestroyWindow(window);
 
     printf("Quit SDL\n");
     SDL_Quit();    
