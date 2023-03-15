a42a40fbd7e75b1f021a747ba3fca760e8d626c5
rackhamn <carl.m.blumenthal@gmail.com>
2023-03-15 02:51:14 +0100

    Added simple us runtime timer

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "time.c"
+
 // $ gcc main.c -o a.out
 int main(int argc, char ** argv) {
 
+    unsigned long long int start, end, elapsed; // us 
+
+    start = get_time_us();
+
     printf("hello world!\n");
+
+    end = get_time_us();
+    elapsed = end - start;
+
+    printf("runtime: %llu (us)\n", elapsed);
 
     return 0;
 }
